66e17569a087f71b6442d8a1bc56c0573694229a
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-02-29 11:35:29 -0800

    Change package name to intent

--- pkg/intent/client.go
+++ pkg/intent/client.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package kubeDSL
+package intent
 
 import (
 	apiextension "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
--- pkg/intent/cluster.go
+++ pkg/intent/cluster.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package kubeDSL
+package intent
 
 import (
 	"github.com/adibrastegarnia/kubeDSL/pkg/kube"
